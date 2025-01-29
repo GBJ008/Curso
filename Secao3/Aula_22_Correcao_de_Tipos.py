72d4edc2477e362706e86e0f267cd49d9e01561f
GBJ008 <gabrielerik140@gmail.com>
2025-01-28 22:57:55 -0300

    Aqui foi adicionado mais exercício da aula, mostrando o tipo de classificações de cada coisa (str,bool,int,float) e como faz a tipagem de cada um

---
+++ Secao3/Aula_22_Correcao_de_Tipos.py
@@ -0,0 +1,18 @@
+#conversão de tipos, correção
+#Type convertion,typecasting, coercion
+#é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str,int,float, bool
+print(1+1)
+# ele vai colquetenar= Juntar ou somar os caracteres
+print('a'+'b')
+#convertudo para int
+print(int('1'), type(int('1')))
+print(int('1')+ 1)
+#converter para float
+print(float('1')+2)
+#convertendo para bolean
+print(bool(''))#False
+print(bool(' '))#True
+#convertendo para String
+print(str(1)+ 'a')
